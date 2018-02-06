fa1124d1003aa3a04de6f97d31aee7e1df461e4a
unknown <jjhernandez94@yahoo.com>
2018-02-06 15:30:00 -0800

    Integrated frontend draft files

--- js/app_joel.js
+++ js/app_joel.js
@@ -5,7 +5,7 @@ console.log("document loaded");
         $('#myModal').modal('show');
     }
 
-    $("#characterInput").on("click", function() {
+    $(".btn").on("click", function() {
         //Prevents the document from reloading
         event.preventDefault();
 
@@ -68,15 +68,41 @@ console.log("document loaded");
             }
 
             if (isMarvel) {
-                $("#characterName").text(characterName);
-                $("#realName").text(character.real_name);
-                $("#birthDate").text(character.birth);
-                $("#description").html(character.description);
-                // $(".slide").html("src", character.image.medium_url);
+                $(".name").text(characterName);
+                $(".realName").text(character.real_name);
+                $(".birth").text(character.birth);
+                $(".description").html(character.description);
+                $("img").attr("src", character.image.medium_url);
             }
         });
     });
 });
+//ALTERNATIVE MODAL CODE
+// Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
 //ALTERNATIVE ALIAS CODE
 // console.log(lcComicName);
